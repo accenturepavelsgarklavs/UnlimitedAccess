3154e7bddc9bfedabba25108d3fcb85592ef2646
Pāvels Garklāvs <pavels.garklavs@accenture.com>
2022-03-03 14:46:07 +0200

    Initial Commit

---
+++ UnlimitedAccess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnlimitedAccess
+//
+//  Created by pavels.garklavs on 03/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
